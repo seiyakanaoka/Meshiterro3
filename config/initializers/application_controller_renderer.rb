5b516c3a9cd01df66684aa530505e25b3c776f7f
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-12 12:29:51 +0000

    [Update]Meshiterro3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9d3f6ac431190ad3b1f1893014a03f0917cbb0896f4b91514803f62a09db09a4510fb55a2ff7ea3f8163c85caba9b6dbfac388998ef6b496cdac197e982ff1ec'
